3ef36cd39b3d77a6df2dbe34cfd827c4a39794b1
Aditya Sharma <adihgr18@gmail.com>
2023-08-24 14:58:26 +0530

    contact model modified

--- models/Contact.js
+++ models/Contact.js
@@ -9,14 +9,11 @@ contactSchema = new Schema({
         unique: true,
         required: true
     },
-    firstName: {
+    name: {
         type: String,
         required: true,
     },
-    lastName: {
-        type: String,
-        required: true,
-    },
+    
     phoneNumber: {
         type: String,
         required: true
